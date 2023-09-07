4dea1c0f76dac6594fa1d5e8044ba05b5cd01112
Jeffrey Hinkle <cheffreyh@gmail.com>
2023-09-07 15:43:06 -0400

    ignoring files

--- github_interface.py
+++
@@ -1,9 +0,0 @@
-import git
-
-lesson_name = "dsc-functions-with-arguments-lab"
-
-repo = f'https://github.com/learn-co-curriculum/{lesson_name}'
-
-repo = git.Repo(repo)
-
-repo.remotes.origin.pull()
